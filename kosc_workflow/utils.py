c0c8a0c4bb7c6f6b181b25bb25c2430126b25a21
Said Ben Rjab <rjabssaid@gmail.com>
2018-11-19 15:25:14 +0100

    Add TestAllTransitions class.

--- kosc_workflow/utils.py
+++ kosc_workflow/utils.py
@@ -22,3 +22,24 @@ class ContextDecorator(object):
 
 class transaction(object):
     atomic = ContextDecorator()
+
+
+class TestAllTransitionsMixin(object):
+    factory_class = None
+    transitions = []
+    ignore_transitions = []
+
+    def _get_test_transitions(self):
+        return [tr for tr in self.transitions if tr["name"] not in self.ignore_transitions]
+
+    def test_all_transitions(self):
+        for transition in self._get_test_transitions():
+            sources = transition["source"] if isinstance(transition["source"], list) else [transition["source"], ]
+
+            for source in sources:
+                order = self.factory_class(state=source)
+
+                getattr(order.workflow, transition["name"])()
+
+                order.refresh_from_db()
+                self.assertEqual(order.state, transition["destination"])
